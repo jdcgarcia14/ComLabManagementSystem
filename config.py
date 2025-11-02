9d5b3f56f5aceab71ca85586c5f7e446a46f3cf1
jdcgarcia14 <146795188+jdcgarcia14@users.noreply.github.com>
2025-11-03 00:26:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-NGROK_URL = "https://unhushable-natalya-ungaraged.ngrok-free.dev"
-=======
 NGROK_URL = "https://comlabmanagementsystem.onrender.com/"
->>>>>>> 2ac5e24b0504009c95a78d98d29e3779d81406a2
